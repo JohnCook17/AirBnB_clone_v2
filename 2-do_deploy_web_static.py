2b0a4e7c6353071acfc2b453893fa73b6f3e0404
JohnCook17 <655@holbertonschool.com>
2019-08-19 19:46:50 -0700

    wrapped in a try except

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,18 +26,17 @@ def do_pack():
 
 def do_deploy(archive_path):
     
-    #try:
-    my_path = archive_path.split(".")[0]
-    my_path = my_path.split("/")[1]
-    put("{}".format(archive_path), "/tmp")
-    run("mkdir -p /data/web_static/releases/{}/".format(my_path))
-    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(my_path, my_path))
-    run("rm -r /tmp/{}.tgz".format(my_path))
-    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(my_path, my_path))
-    run(" rm -rf /data/web_static/releases/{}/web_static/current".format(my_path))
-    run("ln -s /data/web_static/current /data/web_static/releases/{}".format(my_path))
+    try:
+        my_path = archive_path.split(".")[0]
+        my_path = my_path.split("/")[1]
+        put("{}".format(archive_path), "/tmp")
+        run("mkdir -p /data/web_static/releases/{}/".format(my_path))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(my_path, my_path))
+        run("rm -r /tmp/{}.tgz".format(my_path))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(my_path, my_path))
+        run(" rm -rf /data/web_static/releases/{}/web_static/current".format(my_path))
+        run("ln -s /data/web_static/current /data/web_static/releases/{}".format(my_path))
 
-    return True
-    #except:
-        #print("fail")
-        #return False
+        return True
+    except:
+        return False
